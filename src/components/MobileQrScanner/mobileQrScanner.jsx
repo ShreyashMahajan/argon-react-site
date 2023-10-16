2b3d2e302399f12cc2ef88113db1efe14ab65532
ShreyashMahajan <shreyashmahajan98@gmail.com>
2023-10-17 00:26:14 +0530

    version pf react qr scanner updated

--- src/components/MobileQrScanner/mobileQrScanner.jsx
+++ src/components/MobileQrScanner/mobileQrScanner.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {QrReader} from "react-qr-reader";
+import QrReader from "react-qr-reader";
 
 const MobileQrScanner = () => {
   const [startScan, setStartScan] = useState(false);
